232e232d92b93d87909675cd653fce38a9af185c
Amany Mohammed <amany.mohammd@gmail.com>
2024-07-24 00:59:52 +0300

    142. Linked List Cycle II

---
+++ LinkedList/Medium/0142.linked-list-cycle-II.py
@@ -0,0 +1,48 @@
+"""
+142. Linked List Cycle II
+https://leetcode.com/problems/linked-list-cycle-ii/description/
+
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+Do not modify the linked list.
+
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+"""
+from typing import Optional
+
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Slow and faster pointers technique
+        Time complexity: O(n)
+        Space complexity: O(1)
+        """
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            # Cycle found
+            if slow == fast:
+                start = head  # Initialize another pointer to the head
+
+                # Move both pointers at the same speed until they meet at the cycle's start node
+                while start != slow:
+                    start = start.next
+                    slow = slow .next
+
+                return start
