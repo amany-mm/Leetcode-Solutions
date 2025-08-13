e4d25a36920e56a0133ebe2b53e56e9b4aab57ed
Amany Mohammed <amany.mohammd@gmail.com>
2025-08-13 18:51:55 +0300

    145. Binary Tree Postorder Traversal

---
+++ tree/easy/0145_binary_tree_postorder_traversal.py
@@ -0,0 +1,65 @@
+"""
+145. Binary Tree Postorder Traversal
+
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+ 
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,6,7,5,2,9,8,3,1]
+
+Explanation:
+        1
+    /   \
+    2     3
+    / \   / \
+   4   5 6   7
+          \
+           8
+          / \
+         9   null
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+
+Constraints:
+The number of the nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+"""
+
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        Using recursive DFS
+        Time complexity: O(n)
+        Space complexity: O(h), where h is the height of the tree
+        """
+        if not root:
+            return []
+
+        return self.postorderTraversal(root.left) + \
+            self.postorderTraversal(root.right) + [root.val]
